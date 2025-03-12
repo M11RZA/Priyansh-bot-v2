9a942b6d0707ba3dcf8f2a5da95106bba9e906af
M11RZA <Myt94882@gmail.com>
2025-03-12 21:28:29 +0300

    Update and rename 0_ARYAN-BRYANI.js to 0_SAMEER-LOLLIPOP.js

--- Priyansh/commands/0_SAMEER-LOLLIPOP.js
+++ Priyansh/commands/0_SAMEER-LOLLIPOP.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "Bryani",
+  name: "lollipop",
     version: "2.1.1",
   hasPermssion: 0,
   credits: "𝐏𝐑𝐈𝐘𝐀𝐍𝐒𝐇𝐈 𝐊𝐀𝐔𝐑", 
@@ -13,18 +13,18 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("Bryani") ||
-     react.includes("biryani") || react.includes("bryani") || react.includes("BRYANI") ||
-react.includes("Bryani") ||
-react.includes("BRYANI") ||     
-react.includes("bryani")) {
+  if(react.includes("lollipop") ||
+     react.includes("Lollipop") || react.includes("bryani") || react.includes("BRYANI") ||
+react.includes("lolipop") ||
+react.includes("LOLLIPOP") ||     
+react.includes("Lolipopp")) {
     var msg = {
-        body: `${name} 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐀𝐏𝐊𝐈 𝐁𝐑𝐘𝐀𝐍𝐈 💐✿
+        body: `${name} 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐀𝐏𝐊𝐈 𝐋𝐎𝐋𝐋𝐈𝐏𝐎𝐏 💐✿
         •⊰ ☞𓅂 𝐌𝐀𝐃𝐄 ♡ 𝐁𝐘 ♡ 𝐀𝐑𝐘𝐀𝐍𓅂☜ ⊱•`,
 attachment: fs.createReadStream(__dirname + `/noprefix/Bryani.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🍗", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🍭", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
